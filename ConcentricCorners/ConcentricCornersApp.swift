b56dea1a6805f56188667e1ab1c827b50d77b09c
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-01-01 05:14:00 +0300

    Initial Commit

---
+++ ConcentricCorners/ConcentricCornersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentricCornersApp.swift
+//  ConcentricCorners
+//
+//  Created by Abdelrahman Mohamed on 01.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ConcentricCornersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcentricCornersTests/ConcentricCornersTests.swift
@@ -0,0 +1,16 @@
+//
+//  ConcentricCornersTests.swift
+//  ConcentricCornersTests
+//
+//  Created by Abdelrahman Mohamed on 01.01.2026.
+//
+
+import Testing
+
+struct ConcentricCornersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
